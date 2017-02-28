b891a32c1e6de2c735be98c7ecc33e13f70a4abe
Elillos <elia.kobe@hotmail.it>
2017-02-28 16:13:01 +0100

    Hub per la gestione di update simultanei sulla posizione di un Oggetto - Usato per giochi real time

---
+++ MoveShapeHub_SignalR/Model/ShapeModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoveShapeHub_SignalR.Model
+{
+    public class ShapeModel
+    {
+        [JsonProperty("left")]
+        public double Left { get; set; }
+
+        [JsonProperty("top")]
+        public double Top { get; set; }
+
+        [JsonIgnore]
+        public string LastUpdatedBy { get; set; }
+    }
+}
---
+++ MoveShapeHub_SignalR/MoveShapeHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using MoveShapeHub_SignalR.Model;
+
+namespace MoveShapeHub_SignalR
+{
+    public class MoveShapeHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void UpdateShape(ShapeModel shapeModel)
+        {
+            shapeModel.LastUpdatedBy = Context.ConnectionId;
+
+            //Invio le modifiche a tutti tranne all ultimo che ha effettuato la modifica
+            Clients.AllExcept(shapeModel.LastUpdatedBy).sendUpdatedShape(shapeModel);
+        }
+    }
+}
